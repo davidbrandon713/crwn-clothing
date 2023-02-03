d01f00157e0be1417e158b59b5f86609c99323c0
David <davidbrandon713@gmail.com>
2023-02-02 20:32:10 -0600

    Added 'add to cart' functionality 2023-02-02

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import { UserProvider } from './components/contexts/user-context';
-import { ProductsProvider } from './components/contexts/products-context';
-import { CartProvider } from './components/contexts/cart-context';
+import { UserProvider } from './contexts/user-context';
+import { ProductsProvider } from './contexts/products-context';
+import { CartProvider } from './contexts/cart-context';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './index.scss';
