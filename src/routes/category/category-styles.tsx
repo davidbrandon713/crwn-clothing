ea737aafc4c68e708589f2403dbcc0784595728f
David <davidbrandon713@gmail.com>
2023-03-31 18:09:24 -0500

    fix mobile styling bug

--- src/routes/category/category-styles.tsx
+++ src/routes/category/category-styles.tsx
@@ -5,6 +5,12 @@ export const CategoryContainer = styled.div`
   grid-template-columns: repeat(4, 1fr);
   column-gap: 20px;
   row-gap: 50px;
+
+  @media screen and (max-width: 800px) {
+    grid-template-columns: 1fr 1fr;
+    grid-column-gap: 16px;
+    grid-row-gap: 10px;
+  }
 `;
 
 export const CategoryTitle = styled.h2`
